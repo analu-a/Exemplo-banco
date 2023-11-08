7f3d65596343f2b7ddd0391060177107b9f56387
Ana Luiza <analuiza.azevedomorais@gmail.com>
2023-11-08 17:21:44 -0300

    Exemplo com banco

---
+++ src/br/senai/sp/jandira/controller/EmpresaController.java
@@ -0,0 +1,46 @@
+package br.senai.sp.jandira.controller;
+
+import br.senai.sp.jandira.model.Conexao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class EmpresaController {
+
+    public void consultarFuncionarios() throws SQLException {
+
+        //Instanciando conexão com Banco
+
+        Conexao conexao = new Conexao();
+
+        //Representa a conexão com banco de dados no controller
+        Connection objConnection = conexao.getConnection();
+
+        //Responsavel por executar a query(pesquisa)
+
+        Statement statement = objConnection.createStatement();
+
+        //Query
+        String queryConsulta = "select * from funcionarios";
+
+        //Armazena resposta do banco
+        ResultSet resultSet = statement.executeQuery(queryConsulta);
+
+        while (resultSet.next()){
+            int idFuncionario = resultSet.getInt("id_funcionario");
+            String nome = resultSet.getString("nome");
+            String cargo = resultSet.getString("cargo");
+            double salario = resultSet.getDouble("salario");
+            String departamento = resultSet.getString("departamento");
+
+            System.out.println(idFuncionario);
+            System.out.println(nome);
+            System.out.println(cargo);
+            System.out.println(salario);
+            System.out.println(departamento);
+        }
+    }
+
+}
